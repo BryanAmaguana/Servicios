d2e6efb1340b02e621f6f15074d2de2e03d408b5
Bryan Amaguaña <bryanamaguana@MacBook-Pro-de-Bryan.local>
2021-01-06 13:28:04 -0500

    “Prueba”

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,8 @@ process.env.PORT = process.env.PORT || 'dev';
 let urlDB;
 
 if (process.env.PORT === 'dev') {
-    urlDB = 'mongodb://localhost:27017/union';
+    // urlDB = 'mongodb://localhost:27017/union';
+    urlDB = 'mongodb+srv://Bryan:Bryan1996@cluster0.vfala.mongodb.net/union'
 } else {
     urlDB = 'mongodb+srv://Bryan:Bryan1996@cluster0.vfala.mongodb.net/union'
 }
