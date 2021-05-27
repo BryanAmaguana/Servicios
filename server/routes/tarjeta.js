16c1ee3a6110845d7a6c6d2ca639c84eb710465e
BryanAmaguana <77023389+BryanAmaguana@users.noreply.github.com>
2021-05-27 10:58:07 -0500

    Actualizacion

--- server/routes/tarjeta.js
+++ server/routes/tarjeta.js
@@ -108,7 +108,7 @@ app.get("/InicializarTarjeta/:codigo/:valor_tarjeta/:descripcion", function (req
 
     tarjeta.save((err, TarjetaStored) => {
       if (err) {
-  Tarjeta.updateOne({ codigo: codigo }, (err, TarjetaStored) => {
+  Tarjeta.updateOne({ codigo: codigo },{ disponible: true },{valor_tarjeta: 1}, {bloqueo: ""}, (err, TarjetaStored) => {
             if (err) {
                 es.status(404).send({ error: "Error al Inicializar la tarjeta." });
             } else {
